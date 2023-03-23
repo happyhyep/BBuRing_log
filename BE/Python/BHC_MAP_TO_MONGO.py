685d2885cee56e3020ac0da61221cc338ad6b1e8
Sihwan Kim <52437971+van1164@users.noreply.github.com>
2023-03-23 14:58:59 +0900

    ADD Python function
    ADD Python function for inserting BHC_MAP to mongodb

---
+++ BE/Python/BHC_MAP_TO_MONGO.py
@@ -0,0 +1,16 @@
+import requests
+import pymongo
+
+client = pymongo.MongoClient('localhost', 27017)
+db=client.BHC_PROJECT
+collection = db.MAP
+if collection.find_one()==None:
+    REST_API_KEY = 'e0bfedd412178e73dee2a88eae8f3ad0'
+    headers = {'Authorization': "KakaoAK {}".format(REST_API_KEY)}
+    count = 0
+    for page_idx in range(1,45):
+        resData = requests.get("https://dapi.kakao.com/v2/local/search/keyword.json?query=bhc&&page={}".format(page_idx),headers=headers).json()
+        for map in resData['documents']:
+            collection.insert_one(map)
+            count+=1
+    print(count)
