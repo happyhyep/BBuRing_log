90598953f50dbfab2a8969c57abc094469822c3c
van133 <vkn123@naver.com>
2023-03-28 23:47:10 +0900

    Update BHC_MAP_TO_MONGO.py

--- BE/Python/BHC_MAP_TO_MONGO.py
+++ BE/Python/BHC_MAP_TO_MONGO.py
@@ -1,19 +1,5 @@
 import requests
-import pymongo
-
-client = pymongo.MongoClient('localhost', 27017)
-db=client.BHC_PROJECT
-collection = db.MAP
-if collection.find_one()==None:
-    REST_API_KEY = 'e0bfedd412178e73dee2a88eae8f3ad0'
-    headers = {'Authorization': "KakaoAK {}".format(REST_API_KEY)}
-    count = 0
-    for page_idx in range(1,45):
-        resData = requests.get("https://dapi.kakao.com/v2/local/search/keyword.json?query=bhc&&page={}".format(page_idx),headers=headers).json()
-        for map in resData['documents']:
-            collection.insert_one(map)
-            count+=1
-
-
-
-    print(count)
+API_KEY ='8D666D83-E9AC-3A40-9D46-788E56CABC1F'
+resData = requests.get("http://api.vworld.kr/req/search?key={}&&query={}&&request=search&&type=ADDRESS&&category=ROAD".format(API_KEY,'인천 남동구 백범로294번길 19 (간석동, 간석LH1단지상가)')).json()
+loc = resData['response']['result']['items'][0]['point']
+print(loc['x'],loc['y'])
